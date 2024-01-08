5c8720c7902b92f07f61299d69ae014893f5828b
AnJeMin <jmahn@acrofuture.com>
2024-01-08 16:56:16 +0900

    Feat: 로그인 페이지 슬라이드 컴포넌트 퍼블리싱

---
+++ src/components/loginContainer/slide/index.jsx
@@ -0,0 +1,27 @@
+import * as S from '../styled'
+
+const SlideContainer = () => {
+    return (
+        <S.SlideContainer>
+            <S.ImageContainer>
+                <img />
+            </S.ImageContainer>
+            <S.DescriptionContainer>
+                <S.LargeText>AutoEver Global Search Info Service Management</S.LargeText>
+                <S.MediumText>
+                    Enhancing the efficiency and effectiveness of global Search info service
+                    optimization
+                </S.MediumText>
+                <S.SlideButtonContainer>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </S.SlideButtonContainer>
+            </S.DescriptionContainer>
+        </S.SlideContainer>
+    )
+}
+
+export default SlideContainer

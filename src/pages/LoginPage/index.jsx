4baff54515ec3088861c6dfbe40f06bac4cb5293
AnJeMin <jmahn@acrofuture.com>
2024-01-18 17:57:25 +0900

    Feat: 로그인 페이지 구현

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,7 +1,30 @@
-import { Box } from '@mui/material'
+import { Box, Container, Divider, Stack } from '@mui/material'
+import CarouselOverview from '#/pages/LoginPage/CarouselOverview'
+import LoginFormContainer from '#/pages/LoginPage/AuthForm'
 
 function LoginPage() {
-    return <Box>LoginPage</Box>
+    return (
+        <Container
+            maxWidth={'lg'}
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                height: '100vh',
+            }}
+        >
+            <Stack
+                divider={<Divider orientation="vertical" flexItem />}
+                sx={{ flexDirection: 'row', border: '3px double gray' }}
+            >
+                <Box sx={{ flex: 1 }}>
+                    <CarouselOverview />
+                </Box>
+                <Box sx={{ flex: 1 }}>
+                    <LoginFormContainer />
+                </Box>
+            </Stack>
+        </Container>
+    )
 }
 
 export default LoginPage

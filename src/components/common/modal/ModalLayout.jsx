aaee1fabc2611ef824e3f7cbd5d534edd7d9a101
AnJeMin <jmahn@acrofuture.com>
2024-01-19 13:05:01 +0900

    Refactor: ModalLayout 컴포넌트 styled 제거

--- src/components/common/modal/ModalLayout.jsx
+++ src/components/common/modal/ModalLayout.jsx
@@ -1,19 +1,41 @@
-import { IconButton, Modal } from '@mui/material'
+import { Box, IconButton, Modal, Typography } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
-import * as S from './styled'
 
 const ModalLayout = ({ children, isOpen, onClose, title }) => {
     return (
         <Modal open={isOpen} onClose={onClose}>
-            <S.ModalContainer>
-                <S.ModalTitle>
-                    <S.H2>{title}</S.H2>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    bgcolor: '#fff',
+                    width: 600,
+                    minWidth: 600,
+                    maxHeight: 'calc(100vh - 100px)',
+                    overflowY: 'auto',
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        borderBottom: 1,
+                        py: 1,
+                        px: 2,
+                    }}
+                >
+                    <Typography variant="h5">{title}</Typography>
                     <IconButton onClick={onClose}>
-                        <CloseIcon />
+                        <CloseIcon fontSize="large" />
                     </IconButton>
-                </S.ModalTitle>
-                <S.ModalContant>{children}</S.ModalContant>
-            </S.ModalContainer>
+                </Box>
+                <Box sx={{ p: 2 }}>{children}</Box>
+            </Box>
         </Modal>
     )
 }

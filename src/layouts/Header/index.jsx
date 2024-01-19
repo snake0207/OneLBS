b35669180f206d4441dbe4df058e12b5ded651f6
Zerome <unknown.prop@gmail.com>
2024-01-19 16:34:54 +0900

    header layout button 추가

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -10,14 +10,22 @@ import Container from '@mui/material/Container'
 import Avatar from '@mui/material/Avatar'
 import Tooltip from '@mui/material/Tooltip'
 import MenuItem from '@mui/material/MenuItem'
+import SettingsIcon from '@mui/icons-material/Settings'
+import FullscreenIcon from '@mui/icons-material/Fullscreen'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+
+import Badge from '@mui/material/Badge'
 
 import t from '#/common/libs/trans'
+import ArrowButton from '#/components/common/button/ArrowButton'
 
-const settings = [t('profile'), 'Account', 'Dashboard', t('logout')]
+const settings = [t('profile'), t('logout')]
+const languages = [t('kor'), t('eng')]
 
 function Header() {
     const [, setAnchorElNav] = React.useState(null)
     const [anchorElUser, setAnchorElUser] = React.useState(null)
+    const [anchorElLang, setAnchorElLang] = React.useState(null)
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget)
@@ -28,6 +36,14 @@ function Header() {
 
     const handleCloseUserMenu = () => {
         setAnchorElUser(null)
+    }
+
+    const handleOpenLangMenu = (event) => {
+        setAnchorElLang(event.currentTarget)
+    }
+
+    const handleCloseLangMenu = () => {
+        setAnchorElLang(null)
     }
 
     return (
@@ -48,11 +64,17 @@ function Header() {
                     </Box>
                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}></Box>
                     <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                        <Tooltip title="Open notifications">
+                            <IconButton color="inherit" size="large">
+                                <Badge badgeContent={4} color="error">
+                                    <NotificationsIcon />
+                                </Badge>
                             </IconButton>
                         </Tooltip>
+                        <IconButton sx={{ p: 0 }}>
+                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                        </IconButton>
+                        <ArrowButton onClick={handleOpenUserMenu}>James</ArrowButton>
                         <Menu
                             sx={{ mt: '45px' }}
                             id="menu-appbar"
@@ -75,6 +97,36 @@ function Header() {
                                 </MenuItem>
                             ))}
                         </Menu>
+
+                        <ArrowButton onClick={handleOpenLangMenu}>KOR</ArrowButton>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElLang}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElLang)}
+                            onClose={handleCloseLangMenu}
+                        >
+                            {languages.map((lang) => (
+                                <MenuItem key={lang} onClick={handleCloseLangMenu}>
+                                    <Typography textAlign="center">{lang}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                        <IconButton sx={{ p: 0 }}>
+                            <FullscreenIcon />
+                        </IconButton>
+                        <IconButton sx={{ p: 0 }}>
+                            <SettingsIcon />
+                        </IconButton>
                     </Box>
                 </Toolbar>
             </Container>
---
+++ src/pages/DemoPage/ButtonPage/index.jsx
@@ -0,0 +1,15 @@
+import ArrowButton from '#/components/common/button/ArrowButton'
+import { Box } from '@mui/material'
+
+function ButtonPage() {
+    return (
+        <Box>
+            <h1>Buttons</h1>
+            <div>
+                <ArrowButton>Arrow Button</ArrowButton>
+            </div>
+        </Box>
+    )
+}
+
+export default ButtonPage

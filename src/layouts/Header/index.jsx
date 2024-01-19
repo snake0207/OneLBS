c773536157336235159b379b6602bddc54afe1b9
Zerome <unknown.prop@gmail.com>
2024-01-19 17:34:33 +0900

    dropdown component 추가

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -3,13 +3,10 @@ import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
-import Menu from '@mui/material/Menu'
 import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/material/Container'
 import Avatar from '@mui/material/Avatar'
 import Tooltip from '@mui/material/Tooltip'
-import MenuItem from '@mui/material/MenuItem'
 import SettingsIcon from '@mui/icons-material/Settings'
 import FullscreenIcon from '@mui/icons-material/Fullscreen'
 import NotificationsIcon from '@mui/icons-material/Notifications'
@@ -17,33 +14,29 @@ import NotificationsIcon from '@mui/icons-material/Notifications'
 import Badge from '@mui/material/Badge'
 
 import t from '#/common/libs/trans'
-import ArrowButton from '#/components/common/button/ArrowButton'
+import Dropdown from '#/components/common/button/Dropdown'
 
-const settings = [t('profile'), t('logout')]
-const languages = [t('kor'), t('eng')]
+const userMenus = [
+    { key: 'profile', label: t('profile') },
+    { key: 'logout', label: t('logout') },
+]
+const languages = [
+    { key: 'kor', label: t('kor') },
+    { key: 'eng', label: t('eng') },
+]
 
 function Header() {
     const [, setAnchorElNav] = React.useState(null)
-    const [anchorElUser, setAnchorElUser] = React.useState(null)
-    const [anchorElLang, setAnchorElLang] = React.useState(null)
+    const handleSelectUserMenu = (item) => {
+        console.log(item)
+    }
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget)
     }
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget)
-    }
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null)
-    }
-
-    const handleOpenLangMenu = (event) => {
-        setAnchorElLang(event.currentTarget)
-    }
-
-    const handleCloseLangMenu = () => {
-        setAnchorElLang(null)
+    const handleSelectLangMenu = (item) => {
+        console.log(item)
     }
 
     return (
@@ -74,53 +67,16 @@ function Header() {
                         <IconButton sx={{ p: 0 }}>
                             <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
                         </IconButton>
-                        <ArrowButton onClick={handleOpenUserMenu}>James</ArrowButton>
-                        <Menu
-                            sx={{ mt: '45px' }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
+                        <Dropdown items={userMenus} onSelect={handleSelectUserMenu}>
+                            James
+                        </Dropdown>
+                        <Dropdown
+                            items={languages}
+                            selectable={true}
+                            onSelect={handleSelectLangMenu}
                         >
-                            {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    <Typography textAlign="center">{setting}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-
-                        <ArrowButton onClick={handleOpenLangMenu}>KOR</ArrowButton>
-                        <Menu
-                            sx={{ mt: '45px' }}
-                            id="menu-appbar"
-                            anchorEl={anchorElLang}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElLang)}
-                            onClose={handleCloseLangMenu}
-                        >
-                            {languages.map((lang) => (
-                                <MenuItem key={lang} onClick={handleCloseLangMenu}>
-                                    <Typography textAlign="center">{lang}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
+                            KOR
+                        </Dropdown>
                         <IconButton sx={{ p: 0 }}>
                             <FullscreenIcon />
                         </IconButton>
--- src/pages/DemoPage/ButtonPage/index.jsx
+++ src/pages/DemoPage/ButtonPage/index.jsx
@@ -1,13 +1,35 @@
-import ArrowButton from '#/components/common/button/ArrowButton'
+import Dropdown from '#/components/common/button/Dropdown'
 import { Box } from '@mui/material'
 
 function ButtonPage() {
+    const dropItems = [
+        {
+            key: 'item1',
+            label: 'Item 1',
+        },
+        {
+            key: 'item2',
+            label: 'Item 2',
+        },
+        {
+            key: 'item3',
+            label: 'Item 3',
+        },
+    ]
     return (
         <Box>
             <h1>Buttons</h1>
-            <div>
-                <ArrowButton>Arrow Button</ArrowButton>
-            </div>
+            <h2>Dropdown</h2>
+            <div>default</div>
+            <Dropdown items={dropItems}>Dropdown</Dropdown>
+            <div>disabled</div>
+            <Dropdown items={dropItems} disabled>
+                Dropdown
+            </Dropdown>
+            <div>selectable</div>
+            <Dropdown items={dropItems} selectable>
+                Dropdown
+            </Dropdown>
         </Box>
     )
 }

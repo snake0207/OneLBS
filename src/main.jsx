47f6829db569a3879a3c3b0692194645c4affc94
AnJeMin <jmahn@acrofuture.com>
2024-01-23 14:36:40 +0900

    Feat: refresh token 관리하는 쿠키 객체 생성

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import CssBaseline from '@mui/material/CssBaseline'
 import App from './App.jsx'
 import QueryProvider from './components/providers/QueryProvider.jsx'
 import GlobalStyles from './GlobalStyles.jsx'
+import { CookiesProvider } from 'react-cookie'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <QueryProvider>
-            <CssBaseline />
-            <GlobalStyles />
-            <App />
+            <CookiesProvider>
+                <CssBaseline />
+                <GlobalStyles />
+                <App />
+            </CookiesProvider>
         </QueryProvider>
     </React.StrictMode>,
 )

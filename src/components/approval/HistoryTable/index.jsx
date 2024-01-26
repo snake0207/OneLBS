79425aae566c2e1397da8d02196e8ea36049f85d
JinuYong <amazinggal27@gmail.com>
2024-01-26 16:07:35 +0900

    Feat: #38 테이블 컴포넌트 및 검색필터 레이아웃, 페이지네이션 구성

---
+++ src/components/approval/HistoryTable/index.jsx
@@ -0,0 +1,75 @@
+import {
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from '@mui/material'
+import Typography from '@mui/material/Typography'
+import { useModalActions } from '#/store/modalStore.js'
+import { MODAL_TITLE } from '#/contents/constant.js'
+
+const HistoryTable = ({ dummyData, headers }) => {
+    const { openModal } = useModalActions()
+    const handleClickTableRow = ({ target }) => {
+        if (target.tagName === 'TD') {
+            openModal(MODAL_TITLE.detail, target.parentElement.id)
+        }
+    }
+
+    return (
+        <>
+            <Typography>총 {dummyData?.length || 0}건</Typography>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 'max-content' }}>
+                    <TableHead>
+                        <TableRow>
+                            {headers.map((header, index) => (
+                                <TableCell key={index}>{header}</TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {dummyData?.length ? (
+                            dummyData.map((data, index) => {
+                                return (
+                                    <TableRow
+                                        key={data.id}
+                                        id={data.id}
+                                        hover
+                                        onClick={handleClickTableRow}
+                                    >
+                                        <TableCell>{dummyData.length - index}</TableCell>
+                                        <TableCell>{data.name}</TableCell>
+                                        <TableCell>{data.region}</TableCell>
+                                        <TableCell>{data.requester}</TableCell>
+                                        <TableCell>{data.reviewer}</TableCell>
+                                        <TableCell>{data.approver}</TableCell>
+                                        <TableCell>{data.request_date}</TableCell>
+                                        <TableCell>{data.review_date}</TableCell>
+                                        <TableCell>{data.reject_date}</TableCell>
+                                        <TableCell>{data.approval_date}</TableCell>
+                                        <TableCell>{data.status}</TableCell>
+                                        <TableCell>{data.request_reason}</TableCell>
+                                        <TableCell>{data.reject_reason}</TableCell>
+                                        <TableCell>{data.history}</TableCell>
+                                    </TableRow>
+                                )
+                            })
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={headers.length} align={'center'}>
+                                    NO DATA
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    )
+}
+
+export default HistoryTable

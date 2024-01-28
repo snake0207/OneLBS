8b457ad65bd2f18848b22b24462297ec785fb912
강동훈 <dhkang@acrofuture.com>
2024-01-28 17:10:16 +0900

    Feat: gpss 컴포넌트 input 추가및 상태관리 추가

--- src/components/common/map/MapGpssDetail/MapGpssDetailTab/index.jsx
+++ src/components/common/map/MapGpssDetail/MapGpssDetailTab/index.jsx
@@ -13,8 +13,37 @@ import LanguageIcon from '@mui/icons-material/Language'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import EvStationIcon from '@mui/icons-material/EvStation'
 import EditIcon from '@mui/icons-material/Edit'
+import { useFormik } from 'formik'
+import { useState } from 'react'
+import IconButton from '@mui/material/IconButton'
+import SaveIcon from '@mui/icons-material/Save'
+import TextInput from '#/components/common/input/TextInput/index.jsx'
 
 const MapGpssDetailTab = () => {
+    const formik = useFormik({
+        initialValues: {
+            address: '',
+            lat: '',
+            lng: '',
+            reason: '',
+        },
+        onSubmit: (form) => {
+            console.log(form)
+        },
+    })
+
+    const [isAddressSave, setIsAddressSave] = useState(false)
+    const [isLatSave, setIsLatSave] = useState(false)
+    const [isLngSave, setIsLngSave] = useState(false)
+    const handleClickSetAddressState = () => {
+        setIsAddressSave(!isAddressSave)
+    }
+    const handleClickSetLatState = () => {
+        setIsLatSave(!isLatSave)
+    }
+    const handleClickSetLngSate = () => {
+        setIsLngSave(!isLngSave)
+    }
     return (
         <Box sx={{ paddingTop: '16px' }}>
             <Box>
@@ -22,67 +51,115 @@ const MapGpssDetailTab = () => {
             </Box>
             <Divider />
             <Box sx={{ marginTop: '8px', marginBottom: '16px' }}>
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
-                    <Box sx={{ paddingTop: '5px' }}>
-                        <PlaceIcon />
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '6px',
+                    }}
+                >
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Box sx={{ paddingTop: '5px' }}>
+                            <PlaceIcon />
+                        </Box>
+                        <Box>
+                            <Typography>10036 New York, Manhattan, United States</Typography>
+                        </Box>
+                        <IconButton
+                            sx={{
+                                ml: 'auto',
+                                minWidth: '15px',
+                                width: '30px',
+                                minHeight: '15px',
+                                height: '30px',
+                            }}
+                            onClick={handleClickSetAddressState}
+                        >
+                            {isAddressSave ? <SaveIcon /> : <EditIcon />}
+                        </IconButton>
                     </Box>
-                    <Box>
-                        <Typography>10036 New York, Manhattan, United States</Typography>
-                    </Box>
-                    <Button
-                        variant={'contained'}
-                        sx={{
-                            ml: 'auto',
-                            minWidth: '15px',
-                            width: '30px',
-                            minHeight: '15px',
-                            height: '30px',
-                        }}
-                    >
-                        <EditIcon />
-                    </Button>
+                    {(isAddressSave || (!isAddressSave && formik.values.address !== '')) && (
+                        <Box sx={{ height: '40px' }}>
+                            <TextInput
+                                formik={formik}
+                                name={'address'}
+                                IsDisabled={!isAddressSave}
+                                placeholder={'주소를 입력하세요'}
+                            />
+                        </Box>
+                    )}
                 </Box>
                 <Divider sx={{ marginY: '5px' }} />
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
-                    <Box sx={{ paddingTop: '5px' }}>
-                        <LanguageIcon />
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '6px',
+                    }}
+                >
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Box sx={{ paddingTop: '5px' }}>
+                            <LanguageIcon />
+                        </Box>
+                        <Box>
+                            <Typography>40.758077</Typography>
+                        </Box>
+                        <IconButton
+                            sx={{
+                                ml: 'auto',
+                                minWidth: '15px',
+                                width: '30px',
+                                minHeight: '15px',
+                                height: '30px',
+                            }}
+                            onClick={handleClickSetLatState}
+                        >
+                            {isLatSave ? <SaveIcon /> : <EditIcon />}
+                        </IconButton>
                     </Box>
-                    <Box>
-                        <Typography>40.758077</Typography>
-                    </Box>
-                    <Button
-                        variant={'contained'}
-                        sx={{
-                            ml: 'auto',
-                            minWidth: '15px',
-                            width: '30px',
-                            minHeight: '15px',
-                            height: '30px',
-                        }}
-                    >
-                        <EditIcon />
-                    </Button>
+                    {(isLatSave || (!isLatSave && formik.values.lat !== '')) && (
+                        <Box sx={{ height: '40px' }}>
+                            <TextInput
+                                formik={formik}
+                                name={'lat'}
+                                IsDisabled={!isLatSave}
+                                placeholder={'위도를 입력하세요'}
+                            />
+                        </Box>
+                    )}
                 </Box>
                 <Divider sx={{ marginY: '5px' }} />
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
-                    <Box sx={{ paddingTop: '5px' }}>
-                        <LanguageIcon />
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '6px' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Box sx={{ paddingTop: '5px' }}>
+                            <LanguageIcon />
+                        </Box>
+                        <Box>
+                            <Typography>-73.985480</Typography>
+                        </Box>
+                        <IconButton
+                            sx={{
+                                ml: 'auto',
+                                minWidth: '15px',
+                                width: '30px',
+                                minHeight: '15px',
+                                height: '30px',
+                            }}
+                            onClick={handleClickSetLngSate}
+                        >
+                            {isLngSave ? <SaveIcon /> : <EditIcon />}
+                        </IconButton>
                     </Box>
-                    <Box>
-                        <Typography>-73.985480</Typography>
-                    </Box>
-                    <Button
-                        variant={'contained'}
-                        sx={{
-                            ml: 'auto',
-                            minWidth: '15px',
-                            width: '30px',
-                            minHeight: '15px',
-                            height: '30px',
-                        }}
-                    >
-                        <EditIcon />
-                    </Button>
+                    {(isLngSave || (!isLngSave && formik.values.lng !== '')) && (
+                        <Box sx={{ height: '40px' }}>
+                            <TextInput
+                                formik={formik}
+                                name={'lng'}
+                                IsDisabled={!isLngSave}
+                                placeholder={'경도를 입력하세요'}
+                            />
+                        </Box>
+                    )}
                 </Box>
             </Box>
             <Box>

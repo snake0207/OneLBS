5416ee07732c156cc087888c4e8d55f6f0ddc1b0
AnJeMin <jmahn@acrofuture.com>
2024-01-30 17:32:25 +0900

    Fix: API path 상수값 중첩으로 변경

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -2,23 +2,23 @@ import { API_PATH } from '#/contents/api'
 import { getAPI, postAPI } from '../axios'
 
 const postLogin = (data) => {
-    return postAPI({ endPoint: API_PATH.login, data })
+    return postAPI({ endPoint: API_PATH.auth.login, data })
 }
 
 const postJoin = (data) => {
-    return postAPI({ endPoint: API_PATH.join, data })
+    return postAPI({ endPoint: API_PATH.auth.join, data })
 }
 
 const postEmailVerify = (data) => {
-    return postAPI({ endPoint: API_PATH.emailVerify, data })
+    return postAPI({ endPoint: API_PATH.auth.emailVerify, data })
 }
 
 const postVerifyCode = (data) => {
-    return postAPI({ endPoint: API_PATH.verifyCode, data })
+    return postAPI({ endPoint: API_PATH.auth.verifyEmailCode, data })
 }
 
 const getUserIp = () => {
-    return getAPI({ endPoint: API_PATH.get_ip })
+    return getAPI({ endPoint: API_PATH.auth.get_ip })
 }
 
 export default {
--- src/contents/api.js
+++ src/contents/api.js
@@ -1,9 +1,11 @@
 export const API_PATH = {
-    login: '/',
-    join: '/userAuth/api/v1/user/register',
-    confirm_eamil: '/userAuth/api/v1/user/confirm-email',
-    get_ip: '/userAuth/api/v1/user/get-ip',
-    verifyCode: '/',
+    auth: {
+        login: '/',
+        join: '/userAuth/api/v1/user/register',
+        confirm_eamil: '/userAuth/api/v1/user/confirm-email',
+        get_ip: '/userAuth/api/v1/user/get-ip',
+        verifyEmailCode: '/',
+    },
     gpss: {
         reviewer: '/reviewer',
         approver: '/approver',

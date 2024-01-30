b3f3920a24112c267c97c57984bcc02a60e99f21
AnJeMin <jmahn@acrofuture.com>
2024-01-30 14:58:02 +0900

    Feat: 다국어 영어 스크립트 빈 파일 생성

--- src/contents/constant.js
+++ src/contents/constant.js
@@ -1,3 +1,5 @@
+import t from '#/common/libs/trans'
+
 export const AUTH_STEP = {
     imfomation: 0,
     certified: 1,
@@ -7,6 +9,6 @@ export const AUTH_STEP = {
 
 export const MODAL_TITLE = {
     demo: 'Demo',
-    join: '회원가입',
+    join: t('join', 'auth'),
     detail: '상세정보',
 }

62787b6e4e69a253bbcabdba3d1d6223224a44fc
강동훈 <dhkang@acrofuture.com>
2024-01-30 17:47:57 +0900

    Fix: 모달 사이즈 변경할 수 있게 수정

--- src/components/common/modal/index.jsx
+++ src/components/common/modal/index.jsx
@@ -1,9 +1,11 @@
 import { Dialog, DialogTitle, IconButton } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
+import { useModalParamState } from '#/store/useModalStore.js'
 
 const ModalLayout = ({ children, isOpen, onClose, title }) => {
+    const modalSize = useModalParamState()
     return (
-        <Dialog open={isOpen} onClose={onClose} maxWidth="sm">
+        <Dialog open={isOpen} onClose={onClose} maxWidth={modalSize}>
             <DialogTitle
                 sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
             >

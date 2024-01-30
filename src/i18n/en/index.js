f3fe1c7f8ddba52e8d146b5b95d8df381c82d323
강동훈 <dhkang@acrofuture.com>
2024-01-30 13:53:31 +0900

    Feat: gpss / common 국제화 추가

--- src/i18n/en/index.js
+++ src/i18n/en/index.js
@@ -1,9 +1,13 @@
 import layout from './layout.json'
 import users from './users.json'
+import common from './common.json'
+import gpss from './gpss.json'
 
 const en = {
     layout,
     users,
+    common,
+    gpss,
 }
 
 export default en

91b0ca79489a58813287fe733fb7a344df737401
JinuYong <amazinggal27@gmail.com>
2024-01-30 16:52:04 +0900

    Feat: #75 CheckBox Componenet 구성

--- src/i18n/en/index.js
+++ src/i18n/en/index.js
@@ -1,9 +1,11 @@
 import layout from './layout.json'
 import users from './users.json'
+import inputs from './inputs.json'
 
 const en = {
     layout,
     users,
+    inputs,
 }
 
 export default en
--- src/i18n/kr/index.js
+++ src/i18n/kr/index.js
@@ -1,9 +1,11 @@
 import layout from './layout.json'
 import users from './users.json'
+import inputs from './inputs.json'
 
 const kr = {
     layout,
     users,
+    inputs,
 }
 
 export default kr

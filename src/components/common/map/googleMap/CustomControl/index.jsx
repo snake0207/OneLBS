560d1e70a28ee96776fa1270b5b810d01ad2953e
강동훈 <dhkang@acrofuture.com>
2024-01-31 19:59:03 +0900

    Fix: 커스텀 컨트롤러 스타일 적용할 수 있게 수정

--- src/components/common/map/googleMap/CustomControl/index.jsx
+++ src/components/common/map/googleMap/CustomControl/index.jsx
@@ -2,16 +2,19 @@ import { Box } from '@mui/material'
 import { useEffect, useRef } from 'react'
 import { useGoogleMap } from '@react-google-maps/api'
 
-const CustomControl = (props) => {
-    const { children, position } = props
+const CustomControl = ({ position, children, style }) => {
     const map = useGoogleMap()
     const ref = useRef()
     useEffect(() => {
         if (map && ref) {
-            map.controls[new window.google.maps.ControlPosition[position]()].push(ref.current)
+            map.controls[window.google.maps.ControlPosition[position]].push(ref.current)
         }
     }, [map, ref])
-    return <Box ref={ref}>{children}</Box>
+    return (
+        <Box ref={ref} sx={style}>
+            {children}
+        </Box>
+    )
 }
 
 export default CustomControl

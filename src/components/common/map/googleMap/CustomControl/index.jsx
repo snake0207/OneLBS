61beb694de932d3edbbd452412d6ce5b1f7be3d4
강동훈 <dhkang@acrofuture.com>
2024-01-31 13:24:38 +0900

    Feat: google 커스텀 컨트롤러 추가

---
+++ src/components/common/map/googleMap/CustomControl/index.jsx
@@ -0,0 +1,18 @@
+import { Box } from '@mui/material'
+import { useEffect, useRef } from 'react'
+import { useGoogleMap } from '@react-google-maps/api'
+import googleMap from '#/components/common/map/googleMap/index.jsx'
+
+const CustomControl = (props) => {
+    const { children, position } = props
+    const map = useGoogleMap()
+    const ref = useRef()
+    useEffect(() => {
+        if (map && ref) {
+            map.controls[new window.google.maps.ControlPosition[position]()].push(ref.current)
+        }
+    }, [map, ref])
+    return <Box ref={ref}>{children}</Box>
+}
+
+export default CustomControl

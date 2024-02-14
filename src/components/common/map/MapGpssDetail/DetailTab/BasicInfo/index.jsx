3f3cb580d1aa669c39f705d4bfdd3fc324adce62
강동훈 <dhkang@acrofuture.com>
2024-02-14 16:21:04 +0900

    Fix: conflict resolve

--- src/components/common/map/MapGpssDetail/DetailTab/BasicInfo/index.jsx
+++ src/components/common/map/MapGpssDetail/DetailTab/BasicInfo/index.jsx
@@ -1,13 +1,19 @@
 import { Box, Typography } from '@mui/material'
-import PlaceIcon from '@mui/icons-material/Place.js'
 import IconButton from '@mui/material/IconButton'
 import SaveIcon from '@mui/icons-material/Save.js'
-import EditIcon from '@mui/icons-material/Edit.js'
 import t from '#/common/libs/trans.js'
 import Divider from '@mui/material/Divider'
-import LanguageIcon from '@mui/icons-material/Language.js'
 import { useState } from 'react'
 import FormikInput from '#/components/common/input/FormikInput/index.jsx'
+import PointBlueIcon from '#/assets/pointBlueIcon.svg'
+import LanguageIcon from '#/assets/languageIcon.svg'
+import GpsIcon from '#/assets/gpsIcon.svg'
+import EvStationIcon from '#/assets/evStationIcon.svg'
+import PlaceIcon from '@mui/icons-material/Place'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import EditIcon from '@mui/icons-material/Edit'
+import TextInput from '#/components/common/input/TextInput/index.jsx'
+import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material'
 
 const BasicInfo = ({ formik, poiData }) => {
     // 데이터 수정
@@ -25,25 +31,24 @@ const BasicInfo = ({ formik, poiData }) => {
         setIsLngSave(!isLngSave)
     }
     return (
-        <Box>
+        <Box sx={{ paddingTop: '20px' }}>
             <Box>
-                <Typography variant={'h6'}>{poiData.title}</Typography>
+                <Typography variant={'h6'} sx={{ fontSize: '20px', fontWeight: 600 }}>
+                    <img
+                        src={PointBlueIcon}
+                        style={{ verticalAlign: 'middle', paddingRight: '4px' }}
+                    />
+                    Times Square
+                </Typography>
             </Box>
-            <Divider />
             <Box sx={{ marginTop: '8px', marginBottom: '16px' }}>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: '6px',
-                    }}
-                >
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Box>
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
                         <Box sx={{ paddingTop: '5px' }}>
-                            <PlaceIcon />
+                            <img src={LanguageIcon} />
                         </Box>
                         <Box>
-                            <Typography>{poiData.address}</Typography>
+                            <Typography>10036 New York, Manhattan, United States</Typography>
                         </Box>
                         <IconButton
                             sx={{
@@ -58,9 +63,10 @@ const BasicInfo = ({ formik, poiData }) => {
                             {isAddressSave ? <SaveIcon /> : <EditIcon />}
                         </IconButton>
                     </Box>
-                    {isAddressSave && (
+                    {(isAddressSave || (!isAddressSave && formik.values.address !== '')) && (
                         <Box sx={{ height: '40px' }}>
-                            <FormikInput
+                            <TextInput
+                                formik={formik}
                                 name={'address'}
                                 IsDisabled={!isAddressSave}
                                 placeholder={t('address_input', 'gpss')}
@@ -76,12 +82,12 @@ const BasicInfo = ({ formik, poiData }) => {
                         gap: '6px',
                     }}
                 >
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
                         <Box sx={{ paddingTop: '5px' }}>
-                            <LanguageIcon />
+                            <img src={GpsIcon} />
                         </Box>
                         <Box>
-                            <Typography>{poiData.position.center.lat}</Typography>
+                            <Typography>40.758077</Typography>
                         </Box>
                         <IconButton
                             sx={{
@@ -96,11 +102,11 @@ const BasicInfo = ({ formik, poiData }) => {
                             {isLatSave ? <SaveIcon /> : <EditIcon />}
                         </IconButton>
                     </Box>
-                    {isLatSave && (
+                    {(isLatSave || (!isLatSave && formik.values.lat !== '')) && (
                         <Box sx={{ height: '40px' }}>
-                            <FormikInput
+                            <TextInput
                                 formik={formik}
-                                name={'position.center.lat'}
+                                name={'lat'}
                                 IsDisabled={!isLatSave}
                                 placeholder={t('lat_input', 'gpss')}
                             />
@@ -109,12 +115,12 @@ const BasicInfo = ({ formik, poiData }) => {
                 </Box>
                 <Divider sx={{ marginY: '5px' }} />
                 <Box sx={{ display: 'flex', flexDirection: 'column', gap: '6px' }}>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
                         <Box sx={{ paddingTop: '5px' }}>
-                            <LanguageIcon />
+                            <img src={GpsIcon} />
                         </Box>
                         <Box>
-                            <Typography>{poiData.position.center.lon}</Typography>
+                            <Typography>-73.985480</Typography>
                         </Box>
                         <IconButton
                             sx={{
@@ -129,16 +135,41 @@ const BasicInfo = ({ formik, poiData }) => {
                             {isLngSave ? <SaveIcon /> : <EditIcon />}
                         </IconButton>
                     </Box>
-                    {isLngSave && (
+                    {(isLngSave || (!isLngSave && formik.values.lon !== '')) && (
                         <Box sx={{ height: '40px' }}>
-                            <FormikInput
-                                name={'position.center.lon'}
+                            <TextInput
+                                formik={formik}
+                                name={'lon'}
                                 IsDisabled={!isLngSave}
                                 placeholder={t('lon_input', 'gpss')}
                             />
                         </Box>
                     )}
                 </Box>
+                <Divider sx={{ marginY: '5px' }} />
+            </Box>
+            <Box>
+                <Typography sx={{ fontSize: 20, fontWeight: 600, color: '#00418D' }}>
+                    {t('category', 'common')}
+                </Typography>
+            </Box>
+            <Box sx={{ display: 'flex', marginBottom: '16px' }}>
+                <Accordion elevation={0} sx={{ padding: 0 }}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        sx={{ padding: '0px', fontSize: '18px', fontWeight: 500 }}
+                    >
+                        <img
+                            src={EvStationIcon}
+                            style={{ verticalAlign: 'middle', paddingRight: '4px' }}
+                        />
+                        EV Charging
+                    </AccordionSummary>
+                    <AccordionDetails sx={{ padding: 0 }}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </AccordionDetails>
+                </Accordion>
             </Box>
         </Box>
     )

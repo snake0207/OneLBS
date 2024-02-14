26ca2816f432b06ebd93cdcf3669fcc148b87928
AnJeMin <jmahn@acrofuture.com>
2024-02-14 15:49:06 +0900

    Feat: Demo 컴포넌트 작업물 반영

--- src/pages/DemoPage/LoginPage/index.jsx
+++ src/pages/DemoPage/LoginPage/index.jsx
@@ -1,25 +1,26 @@
 import AuthStepper from '#/components/auth/AuthStepper'
 import CarouselOverview from '#/components/auth/CarouselOverview'
 import CertifiedForm from '#/components/auth/authForm/CertifiedForm'
+import EmailAuthForm from '#/components/auth/authForm/EmailAuthForm'
 import LoginForm from '#/components/auth/authForm/LoginForm'
 import PasswordResetForm from '#/components/auth/authForm/PasswordResetForm'
 
 const LoginDemoPage = () => {
     const dummyImageList = [
         {
-            imgUrl: '',
+            imgUrl: './assets/login-img1.svg',
             title: 'AutoEver Global Search Info Service Management 1',
             description:
                 'Enhancing the efficiency and effectiveness of global Search info service optimization',
         },
         {
-            imgUrl: '',
+            imgUrl: './assets/login-img2.svg',
             title: 'AutoEver Global Search Info Service Management 2',
             description:
                 'Enhancing the efficiency and effectiveness of global Search info service optimization',
         },
         {
-            imgUrl: '',
+            imgUrl: './assets/login-img3.svg',
             title: 'AutoEver Global Search Info Service Management 3',
             description:
                 'Enhancing the efficiency and effectiveness of global Search info service optimization',
@@ -41,7 +42,7 @@ const LoginDemoPage = () => {
                         display: 'flex',
                         flexDirection: 'column',
                         flexShrink: 0,
-                        width: 600,
+                        width: 400,
                         height: '100%',
                     }}
                 >
@@ -52,7 +53,7 @@ const LoginDemoPage = () => {
                         display: 'flex',
                         flexDirection: 'column',
                         flexShrink: 0,
-                        width: 600,
+                        width: 400,
                         height: '100%',
                     }}
                 >
@@ -63,11 +64,22 @@ const LoginDemoPage = () => {
                         display: 'flex',
                         flexDirection: 'column',
                         flexShrink: 0,
-                        width: 600,
+                        width: 400,
                         height: '100%',
                     }}
                 >
                     <PasswordResetForm />
+                </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        flexShrink: 0,
+                        width: 400,
+                        height: '100%',
+                    }}
+                >
+                    <EmailAuthForm />
                 </div>
             </div>
         </div>

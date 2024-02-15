dea82a9aea5954aeae18b26ba91879a7f420dd25
강동훈 <dhkang@acrofuture.com>
2024-02-15 15:18:13 +0900

    Fix: 구글 지도 검색 / 리스트 컴포넌트 합침

--- src/components/common/map/googleMap/index.jsx
+++ src/components/common/map/googleMap/index.jsx
@@ -354,23 +354,18 @@ const GoogleMapComponent = ({
                 </CustomControl>
                 {/* poi / gpss 조회 공통 */}
                 {(isPoiSearch || isGpssSearch) && (
-                    <>
+                    <CustomControl position="TOP_LEFT" style={{ left: '0px !important' }}>
+                        {/*<Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>*/}
                         {/* 지도 검색 */}
-                        <CustomControl position="TOP_LEFT" style={{ left: '0px !important' }}>
-                            <MapSearch />
-                        </CustomControl>
+                        <MapSearch />
                         {/* 검색 결과 */}
-                        <CustomControl
-                            position="TOP_LEFT"
-                            style={{ left: '0px !important', top: '320px !important' }}
-                        >
-                            <MapSearchList
-                                searchResultArr={mapSampleData}
-                                selectedPoi={selectedPoi}
-                                setSelectedPoi={setSelectedPoi}
-                            />
-                        </CustomControl>
-                    </>
+                        <MapSearchList
+                            searchResultArr={mapSampleData}
+                            selectedPoi={selectedPoi}
+                            setSelectedPoi={setSelectedPoi}
+                        />
+                        {/*</Box>*/}
+                    </CustomControl>
                 )}
 
                 {/* 상세정보 */}

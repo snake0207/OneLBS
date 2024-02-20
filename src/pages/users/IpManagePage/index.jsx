cc6179792eddddc7f28e0d6510d2a2b0b8a93b18
Zerome <unknown.prop@gmail.com>
2024-02-20 14:48:14 +0900

    user permission history page 추가

--- src/pages/users/IpManagePage/index.jsx
+++ src/pages/users/IpManagePage/index.jsx
@@ -1,7 +1,86 @@
-import { Box } from '@mui/material'
+import { useState } from 'react'
+
+import IpTabel from '#/components/users/access/IpTable'
+import SearchFilter from '#/components/users/access/SearchFilter'
+import t from '#/common/libs/trans'
+
+import { usePopupActions } from '#/store/usePopupStore'
+import AddIpDialog from '#/components/users/access/AddIpDialog'
+import { Box, Button } from '@mui/material'
+
+import users from '#/mock/data/user_ip_list.json'
+import TitleBar from '#/components/common/menu/TitleBar'
 
 function IpManagePage() {
-    return <Box>Ip Management Page</Box>
+    const actions = usePopupActions()
+    const [open, setOpen] = useState(false)
+    const [selectedUser, setSelectedUser] = useState(null)
+
+    const handleSearch = (values) => {
+        console.log('handleSearch', values)
+    }
+
+    const handleEdit = (values) => {
+        console.log('handleEdit', values)
+    }
+
+    const handleDelete = (values) => {
+        console.log('handleDelete', values)
+
+        actions.showPopup(
+            'confirm',
+            t('popup_confirm_delete', 'users'),
+            handleConfirmDelete,
+            () => {},
+        )
+    }
+
+    const handleConfirmDelete = () => {
+        console.log('handleConfirmDelete')
+
+        actions.showPopup('alert', t('popup_confirm_delete_success', 'users'))
+    }
+
+    const handleChangePage = (page) => {
+        console.log('handleChangePage', page)
+    }
+
+    const handleOpenRegisterIp = () => {
+        setSelectedUser(users[0])
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const handleRegister = (values) => {
+        console.log('handleRegister', values)
+
+        setOpen(false)
+    }
+
+    return (
+        <Box>
+            <TitleBar title={t('top_menu.user_ip_access_management')} />
+            <SearchFilter onSearch={handleSearch} />
+            <IpTabel
+                rows={users}
+                onEdit={handleEdit}
+                onDelete={handleDelete}
+                onChangePage={handleChangePage}
+            />
+            <Button variant="contained" onClick={handleOpenRegisterIp}>
+                {t('register_ip', 'users')}
+            </Button>
+            <AddIpDialog
+                user={selectedUser}
+                open={open}
+                onClose={handleClose}
+                onRegister={handleRegister}
+            />
+        </Box>
+    )
 }
 
 export default IpManagePage
--- src/pages/users/PermissionHistoryPage/index.jsx
+++ src/pages/users/PermissionHistoryPage/index.jsx
@@ -1,7 +1,42 @@
 import { Box } from '@mui/material'
+import SearchFilter from '#/components/users/permission/SearchFilter'
+import GeneralTable from '#/components/common/table/GeneralTable'
+import t from '#/common/libs/trans'
+
+import permissionHistory from '#/mock/data/user_permission_history.json'
+import TitleBar from '#/components/common/menu/TitleBar'
 
 function PermissionHistoryPage() {
-    return <Box>PermissionHistoryPage</Box>
+    const handleSearch = (values) => {
+        console.log('handleSearch', values)
+    }
+
+    const handlePageChange = (page) => {
+        console.log(`page: ${page}`)
+    }
+
+    const createColumns = () => {
+        return [
+            { field: 'id', headerName: t('no', 'users'), width: 70 },
+            { field: 'email', headerName: t('email', 'users'), width: 200 },
+            { field: 'name', headerName: t('name', 'users'), width: 130 },
+            { field: 'permission', headerName: t('permission', 'users'), width: 130 },
+            { field: 'update_date', headerName: t('update_date', 'users'), width: 200 },
+            { field: 'update_history', headerName: t('update_history', 'users'), width: 280 },
+        ]
+    }
+
+    return (
+        <Box>
+            <TitleBar title={t('top_menu.user_permission_history')} />{' '}
+            <SearchFilter onSearch={handleSearch} />
+            <GeneralTable
+                columns={createColumns()}
+                rows={permissionHistory}
+                onPageChange={handlePageChange}
+            />
+        </Box>
+    )
 }
 
 export default PermissionHistoryPage

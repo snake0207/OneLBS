8e5820505d05c60b6b3c7e3102814171a0b5af13
AnJeMin <jmahn@acrofuture.com>
2024-02-26 14:12:32 +0900

    Fix: refresh token 요청 훅 제거

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -21,15 +21,10 @@ const getUserIp = () => {
     return getAPI({ endPoint: API_PATH.auth.get_ip })
 }
 
-const postRefreshToken = (data) => {
-    return postAPI({ endPoint: API_PATH.auth.refresh_token, data })
-}
-
 export default {
     postLogin,
     postJoin,
     postVerifyEmail,
     postConfirmEmail,
     getUserIp,
-    postRefreshToken,
 }

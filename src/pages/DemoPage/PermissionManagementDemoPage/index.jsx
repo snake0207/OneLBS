47d4c0bdca49d9ccbcb6a6e427d3aaa5ffaa821d
AnJeMin <jmahn@acrofuture.com>
2024-02-26 18:08:51 +0900

    Feat: permission demo page에 컴포넌트 추가

--- src/pages/DemoPage/PermissionManagementDemoPage/index.jsx
+++ src/pages/DemoPage/PermissionManagementDemoPage/index.jsx
@@ -1,5 +1,16 @@
+import PermissionCard from '#/components/permission/PermissionCard'
+import PermissionLabel from '#/components/permission/PermissionCard/PermissionLabel'
+
 const PermissionManagementDemoPage = () => {
-    return <div></div>
+    return (
+        <div>
+            <h1>Permission Card</h1>
+            <h2>Card</h2>
+            <PermissionCard />
+            <h2>Label</h2>
+            <PermissionLabel />
+        </div>
+    )
 }
 
 export default PermissionManagementDemoPage

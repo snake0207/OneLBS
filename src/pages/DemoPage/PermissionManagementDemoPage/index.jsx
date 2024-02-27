ee54ac34e816cca0880bbe80d2f132f29113e691
AnJeMin <jmahn@acrofuture.com>
2024-02-27 13:46:32 +0900

    Feat: 권한 demo page update

--- src/pages/DemoPage/PermissionManagementDemoPage/index.jsx
+++ src/pages/DemoPage/PermissionManagementDemoPage/index.jsx
@@ -2,13 +2,21 @@ import PermissionCard from '#/components/permission/PermissionCard'
 import PermissionLabel from '#/components/permission/PermissionCard/PermissionLabel'
 
 const PermissionManagementDemoPage = () => {
+    const permissionCardMockData = {
+        roleId: 25,
+        permission: ['M131R', 'M151R', 'M152R', 'M153R', 'M154R', 'M111R', 'M111U', 'M121R'],
+        count: 50,
+    }
+
+    const permissionLabelMockData = 'M131R'
+
     return (
         <div>
             <h1>Permission Card</h1>
             <h2>Card</h2>
-            <PermissionCard />
+            <PermissionCard permissionCard={permissionCardMockData} />
             <h2>Label</h2>
-            <PermissionLabel />
+            <PermissionLabel permission={permissionLabelMockData} />
         </div>
     )
 }

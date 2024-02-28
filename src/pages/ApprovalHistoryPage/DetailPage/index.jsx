8cf94ad7b427c2dd0bf051633e9c09a89ca97adf
JinuYong <amazinggal27@gmail.com>
2024-02-28 09:38:03 +0900

    Fix: component 이름 변경

--- src/pages/ApprovalHistoryPage/DetailPage/index.jsx
+++ src/pages/ApprovalHistoryPage/DetailPage/index.jsx
@@ -18,7 +18,7 @@ import InfoTab from '#/components/approval/Detail/InfoTab/index.jsx'
 import EvChargingInfo from '#/components/approval/Detail/CategoryInfo/EvChargingInfo/index.jsx'
 import { getUserTypeFromPath } from '#/common/libs/approval.js'
 import FuelInfo from '#/components/approval/Detail/CategoryInfo/FuelInfo/index.jsx'
-import H2ChargingInfo from '#/components/approval/Detail/CategoryInfo/H2Charging/index.jsx'
+import H2ChargingInfo from '#/components/approval/Detail/CategoryInfo/H2ChargingInfo/index.jsx'
 
 const ApprovalHistoryDetailPage = () => {
     const params = useParams()

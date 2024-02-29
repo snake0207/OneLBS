616627eb0949041f8072bda7021d43524901f4f8
AnJeMin <jmahn@acrofuture.com>
2024-02-29 18:18:00 +0900

    Feat: token refresh api 연동

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import useAuthStore from '#/store/useAuthStore'
+import auth from '#/api/auth'
 
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_GW_BASE_URL,
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: `${useAuthStore.getState().accesstoken}`,
+        Authorization: `Bearer ${useAuthStore.getState().accesstoken}`,
     },
     withCredentials: true,
 })
@@ -17,7 +18,21 @@ axiosInstance.interceptors.request.use(
 
 axiosInstance.interceptors.response.use(
     (response) => response,
-    (error) => Promise.reject(error),
+    async (error) => {
+        if (error.response.status) {
+            const prevRequest = error.config
+            prevRequest._retry = true
+
+            const newAccessToken = await auth.postRenewToken()
+
+            prevRequest.headers.Authorization = `Bearer ${newAccessToken}`
+            useAuthStore.setState({ accessToken: newAccessToken })
+
+            return axiosInstance(prevRequest)
+        }
+
+        return Promise.reject(error)
+    },
 )
 
 export const getAPI = ({ endPoint, axiosOption }) => {

227a6d9eebbda47507b6c415953637dce2acea23
JinuYong <amazinggal27@gmail.com>
2024-03-04 16:37:27 +0900

    Fix: 상세 하단 액션버튼 타입별 분리생성하도록 수정

--- src/components/approval/Detail/ActionButtons/index.jsx
+++ src/components/approval/Detail/ActionButtons/index.jsx
@@ -1,59 +1,72 @@
-import { Box, Button } from '@mui/material'
+import { Button } from '@mui/material'
 import t from '#/common/libs/trans.js'
 
 const ActionButtons = ({ type, status, clickAction, id }) => {
-    console.log('USER TYPE >> ', type)
-    const buttonTextByType = (type, status) => {
-        switch (type) {
-            case 'requester':
-                return status === 'request'
-                    ? ['retrieve']
-                    : status === 'rejected_review' || status === 'temporary'
-                      ? ['re_request']
-                      : null
-            case 'reviewer':
-                return status === 'request'
-                    ? ['approval', 'reject']
-                    : status === 'reviewed'
-                      ? ['retrieve']
-                      : null
-            case 'approver':
-                return status === 'reviewed' ? ['approval', 'reject'] : null
-        }
-    }
-
-    // 종류에 따른 버튼색상 설정
-    const renderButtonColor = (action) => {
-        switch (action) {
-            case 'approval':
-                return 'button.lightblue'
-            case 'reject':
-                return 'button.lightblue'
-            case 'retrieve':
-                return 'button.lightblue'
-            case 're_request':
-                return 'button.lightblue'
-        }
-    }
-
-    const MakeButtons = (actions) => {
-        return (
-            actions &&
-            actions.map((action, index) => {
-                return (
+    switch (type) {
+        case 'requester':
+            return status === 'request' ? (
+                <Button
+                    variant="contained"
+                    onClick={() => clickAction('retrieve', id)}
+                    sx={{ mr: '4px' }}
+                >
+                    {t(`actions.retrieve`, 'approval')}
+                </Button>
+            ) : status === 'rejected_review' || status === 'temporary' ? (
+                <Button
+                    variant="contained"
+                    onClick={() => clickAction('re_request', id)}
+                    sx={{ mr: '4px' }}
+                >
+                    {t(`actions.re_request`, 'approval')}
+                </Button>
+            ) : null
+        case 'reviewer':
+            return status === 'request' ? (
+                <>
                     <Button
-                        key={index}
                         variant="contained"
-                        onClick={() => clickAction(action, id)}
-                        sx={{ bgcolor: renderButtonColor(action), mr: '4px' }}
+                        onClick={() => clickAction('approval', id)}
+                        sx={{ mr: '4px' }}
                     >
-                        {t(`actions.${action}`, 'approval')}
+                        {t(`actions.approval`, 'approval')}
                     </Button>
-                )
-            })
-        )
+                    <Button
+                        variant="contained"
+                        onClick={() => clickAction('reject', id)}
+                        sx={{ mr: '4px' }}
+                    >
+                        {t(`actions.reject`, 'approval')}
+                    </Button>
+                </>
+            ) : status === 'reviewed' ? (
+                <Button
+                    variant="contained"
+                    onClick={() => clickAction('retrieve', id)}
+                    sx={{ mr: '4px' }}
+                >
+                    {t(`actions.retrieve`, 'approval')}
+                </Button>
+            ) : null
+        case 'approver':
+            return status === 'reviewed' ? (
+                <>
+                    <Button
+                        variant="contained"
+                        onClick={() => clickAction('approval', id)}
+                        sx={{ mr: '4px' }}
+                    >
+                        {t(`actions.approval`, 'approval')}
+                    </Button>
+                    <Button
+                        variant="contained"
+                        onClick={() => clickAction('reject', id)}
+                        sx={{ mr: '4px' }}
+                    >
+                        {t(`actions.reject`, 'approval')}
+                    </Button>
+                </>
+            ) : null
     }
-
-    return <Box>{MakeButtons(buttonTextByType(type, status))}</Box>
 }
 export default ActionButtons

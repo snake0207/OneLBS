0b6e88a88b22820483a8432e8dc0edeb7d70be4d
AnJeMin <jmahn@acrofuture.com>
2024-03-04 17:20:56 +0900

    Feat: OTP 인증 api 연동 (WIP)

--- src/components/auth/authForm/CertifiedForm/index.jsx
+++ src/components/auth/authForm/CertifiedForm/index.jsx
@@ -1,21 +1,26 @@
 import { useState } from 'react'
 import { useFormik } from 'formik'
-import { Box, Button, TextField, Typography, Icon } from '@mui/material'
 import { BrowserView } from 'react-device-detect'
 import CopyToClipboard from 'react-copy-to-clipboard'
-import LoginIcon from '#/assets/loginIcon.svg'
+import { Box, Button, TextField, Typography, Icon } from '@mui/material'
+import { LoadingButton } from '@mui/lab'
 import TextInput from '#/components/common/input/TextInput'
 import { otpSchema } from '#/contents/validationSchema'
 import AuthStepper from '#/components/auth/AuthStepper'
 import FlexEndButtonContainer from '#/components/common/button/FlexEndButtonContainer'
 import OtpGuideModal from '#/components/auth/authForm/CertifiedForm/otpGuideModal'
+import { usePostVerifyOtp } from '#/hooks/queries/auth'
+import useOtpStore from '#/store/useOtpStore'
 
 import t from '#/common/libs/trans'
 
 import style from './style.module'
+import LoginIcon from '#/assets/loginIcon.svg'
 
 const CertifiedForm = () => {
     const [isOtpGuideOpen, setIsOtpGuideOpen] = useState(false)
+    const { mutate, isPending } = usePostVerifyOtp()
+    const { twoFactorAuth, secretKey, twoFactorSecret, qrCodeUrl } = useOtpStore()
 
     const handleClickOtpGuideClose = () => {
         setIsOtpGuideOpen(false)
@@ -27,11 +32,18 @@ const CertifiedForm = () => {
 
     const formik = useFormik({
         initialValues: {
-            otp: '',
+            code: '',
         },
         validationSchema: otpSchema,
         onSubmit: (form) => {
-            console.log(form)
+            mutate(
+                { ...form, twoFactorSecret },
+                {
+                    onSuccess: (data) => {
+                        console.log(data)
+                    },
+                },
+            )
         },
     })
 
@@ -48,56 +60,75 @@ const CertifiedForm = () => {
             <BrowserView>
                 <AuthStepper />
             </BrowserView>
-            <Typography variant="subtitle2" sx={style.subText}>
-                {t('guide.otp_registration_guide', 'auth')}
-            </Typography>
-            <Box sx={{ display: 'flex', gap: 1 }}>
-                <Box sx={{ flexShrink: 0, width: 80, height: 80, bgcolor: 'gray' }} />
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                        alignItems: 'flex-start',
-                    }}
-                >
-                    <Typography variant="subtitle2" sx={style.contentText}>
-                        {t('guide.otp_input_guide', 'auth')}
+            {twoFactorAuth === 'N' && (
+                <>
+                    <Typography variant="subtitle2" sx={style.subText}>
+                        {t('guide.otp_registration_guide', 'auth')}
                     </Typography>
                     <Box sx={{ display: 'flex', gap: 1 }}>
-                        <TextField size="small" value={'123123'} />
-                        <CopyToClipboard text="123123">
-                            <Button
-                                variant="contained"
-                                sx={{
-                                    bgcolor: 'button.gray',
-                                    '&:hover': {
-                                        backgroundColor: 'button.gray',
-                                    },
-                                }}
-                            >
-                                {t('copy', 'auth')}
+                        <img
+                            src={qrCodeUrl}
+                            style={{
+                                flexShrink: 0,
+                                width: 80,
+                                height: 80,
+                                backgroundColor: 'white',
+                            }}
+                        />
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'space-between',
+                                alignItems: 'flex-start',
+                            }}
+                        >
+                            <Typography variant="subtitle2" sx={style.contentText}>
+                                {t('guide.otp_input_guide', 'auth')}
+                            </Typography>
+                            <Box sx={{ display: 'flex', gap: 1 }}>
+                                <TextField size="small" value={secretKey} />
+                                <CopyToClipboard text={secretKey}>
+                                    <Button
+                                        variant="contained"
+                                        sx={{
+                                            bgcolor: 'button.gray',
+                                            '&:hover': {
+                                                backgroundColor: 'button.gray',
+                                            },
+                                        }}
+                                    >
+                                        {t('copy', 'auth')}
+                                    </Button>
+                                </CopyToClipboard>
+                            </Box>
+                            <Button onClick={handleClickOtpGuideOpen} sx={style.linklText}>
+                                {t('guide.otp_registration_button', 'auth')}
                             </Button>
-                        </CopyToClipboard>
+                        </Box>
                     </Box>
-                    <Button onClick={handleClickOtpGuideOpen} sx={style.linklText}>
-                        {t('guide.otp_registration_button', 'auth')}
-                    </Button>
-                </Box>
-            </Box>
+                </>
+            )}
             <Typography variant="h6" sx={style.labelText}>
                 OTP
             </Typography>
-            <TextInput name={'otp'} placeholder={t('placeholder.otp', 'auth')} formik={formik} />
-            <FlexEndButtonContainer>
-                <Button
-                    variant="contained"
-                    onClick={formik.handleSubmit}
-                    sx={{ bgcolor: 'button.main', width: '100%', fontWeight: 400 }}
-                >
-                    {t('certified', 'auth')}
-                </Button>
-            </FlexEndButtonContainer>
+            <form onSubmit={formik.handleSubmit}>
+                <TextInput
+                    name={'code'}
+                    placeholder={t('placeholder.otp', 'auth')}
+                    formik={formik}
+                />
+                <FlexEndButtonContainer>
+                    <LoadingButton
+                        loading={isPending}
+                        variant="contained"
+                        type="submit"
+                        sx={{ bgcolor: 'button.main', width: '100%', fontWeight: 400 }}
+                    >
+                        {t('certified', 'auth')}
+                    </LoadingButton>
+                </FlexEndButtonContainer>
+            </form>
             <OtpGuideModal isOpen={isOtpGuideOpen} handleClose={handleClickOtpGuideClose} />
         </>
     )

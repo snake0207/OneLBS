850da4e3b0e7a4c1767beac52674aedd51dde54d
AnJeMin <jmahn@acrofuture.com>
2024-03-04 14:43:22 +0900

    Feat: 로그인 5회 오류 발생시 오류 처리

--- src/hooks/useApiError.js
+++ src/hooks/useApiError.js
@@ -1,7 +1,12 @@
 import { usePopupActions } from '#/store/usePopupStore'
+import { useAuthStepActions } from '#/store/useAuthStepStore'
+
+import t from '#/common/libs/trans'
+import { AUTH_STEP } from '#/contents/constant'
 
 const useApiError = () => {
     const { showPopup } = usePopupActions()
+    const { changeAuthStep } = useAuthStepActions()
 
     const handlerDefault = () => {
         showPopup('alert', '500 서버 오류')
@@ -11,10 +16,15 @@ const useApiError = () => {
         showPopup('alert', errorMessage)
     }
 
+    const handler40010122 = () => {
+        showPopup('alert', t('400.10122', 'error'), () => changeAuthStep(AUTH_STEP.emailAuth))
+    }
+
     const handlers = {
         default: handlerDefault,
         400: {
             default: handler400Default,
+            10122: handler40010122,
         },
     }
 

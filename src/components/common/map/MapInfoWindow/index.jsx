f638a67632a5929a4e3125a665d55c6efe7b289e
강동훈 <dhkang@acrofuture.com>
2024-03-05 20:15:25 +0900

    Fix: 구글 장소 마커 디자인 반영

--- src/components/common/map/MapInfoWindow/index.jsx
+++ src/components/common/map/MapInfoWindow/index.jsx
@@ -1,57 +1,28 @@
-import { Grid, Typography } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import LanguageIcon from '@mui/icons-material/Language'
 
 const MapInfoWindow = ({ markerData }) => {
     return (
         markerData && (
-            <Grid
-                container
+            <Box
                 sx={{
-                    width: '300px',
-                    height: '100px',
-                    borderRadius: '8px',
-                    border: '1px solid #D1D1D1',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
                 }}
             >
-                <Grid
-                    item
-                    xs={12}
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        borderBottom: '1px solid #D1D1D1',
-                    }}
-                >
-                    <Typography>{markerData ? markerData.title : 'Times Square'}</Typography>
-                </Grid>
-                <Grid
-                    item
-                    xs={6}
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        background: '#D1D1D1',
-                    }}
-                >
-                    <LanguageIcon />
-                    {markerData ? markerData.position.center.lat.toFixed(7) : '40.758077'}
-                </Grid>
-                <Grid
-                    item
-                    xs={6}
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        background: '#D1D1D1',
-                    }}
-                >
-                    <LanguageIcon />
-                    {markerData ? markerData.position.center.lon.toFixed(7) : '-73.98548'}
-                </Grid>
-            </Grid>
+                <Box>
+                    <Typography sx={{ fontSize: '13px', fontWeight: 600, color: '#444' }}>
+                        {markerData.title}
+                    </Typography>
+                </Box>
+                <Box>
+                    <Typography sx={{ fontSize: '12px', color: '#444' }}>
+                        {markerData.position.center.lat.toFixed(7)},
+                        {markerData.position.center.lon.toFixed(7)}
+                    </Typography>
+                </Box>
+            </Box>
         )
     )
 }

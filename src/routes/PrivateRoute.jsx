31974efb5ff75a68efa16a1648e1154b3d2bbffc
강동훈 <dhkang@acrofuture.com>
2024-03-05 15:17:37 +0900

    Chore: 테스트용 로그인 페이지 이동 비활성화

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,8 +2,8 @@ import { useAccessTokenState } from '#/store/useAuthStore'
 import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRoute = () => {
-    const accessToken = useAccessTokenState()
-
+    // const accessToken = useAccessTokenState()
+    const accessToken = true // TODO 테스트용
     return accessToken ? <Outlet /> : <Navigate to={'/login'} />
 }
 

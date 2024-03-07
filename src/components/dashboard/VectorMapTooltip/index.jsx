08477e93ab7d71b6dc5b59a81a76691c605aa706
AnJeMin <jmahn@acrofuture.com>
2024-03-07 16:26:26 +0900

    Feat: vector map tooltip carousel 구현

--- src/components/dashboard/VectorMapTooltip/index.jsx
+++ src/components/dashboard/VectorMapTooltip/index.jsx
@@ -1,27 +1,45 @@
-import { Box, Typography } from '@mui/material'
+import { Box, IconButton, Typography } from '@mui/material'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 import CategoryCount from '#/components/dashboard/CategoryCount'
 import useLayoutStore from '#/store/useLayoutStore'
 import LanguageIcon from '#/assets/languagesIconDark.svg'
+import useEmblaCarousel from 'embla-carousel-react'
+import { useCarouselPrevNextButtons } from '#/hooks/useCarouselPrevNextButtons'
 
 import style from './style.module'
 
 const VectorMapTooltip = ({ title, categoryCountList }) => {
     const { themeMode } = useLayoutStore()
+    const [emblaRef, emblaApi] = useEmblaCarousel({ align: 'start' })
+    const { onNextbuttonClick, onPrevButtonClick, nextBtnDisabled, prevBtnDisabled } =
+        useCarouselPrevNextButtons(emblaApi)
+
     return (
         <Box sx={style.dashboardBox}>
             <Box sx={style.Title}>
                 <img src={LanguageIcon} style={{ width: '20px' }} />
                 <Typography>{title}</Typography>
             </Box>
-            {categoryCountList.map((item) => (
-                <Box key={item.category} sx={style.items}>
-                    <CategoryCount
-                        categoryName={item.category}
-                        icon={themeMode === 'light' ? item.lightIcon : item.darkIcon}
-                        count={item.count}
-                    />
+            <Box ref={emblaRef} sx={{ overflow: 'hidden' }}>
+                <Box sx={{ display: 'flex' }}>
+                    {categoryCountList.map((item) => (
+                        <Box key={item.category} sx={style.items}>
+                            <CategoryCount
+                                categoryName={item.category}
+                                icon={themeMode === 'light' ? item.lightIcon : item.darkIcon}
+                                count={item.count}
+                            />
+                        </Box>
+                    ))}
                 </Box>
-            ))}
+                <IconButton onClick={onPrevButtonClick} disabled={prevBtnDisabled}>
+                    <ArrowBackIosIcon sx={{ width: 20 }} />
+                </IconButton>
+                <IconButton onClick={onNextbuttonClick} disabled={nextBtnDisabled}>
+                    <ArrowForwardIosIcon sx={{ width: 20 }} />
+                </IconButton>
+            </Box>
         </Box>
     )
 }
--- src/components/dashboard/VectorMapTooltip/style.module.js
+++ src/components/dashboard/VectorMapTooltip/style.module.js
@@ -1,15 +1,11 @@
 const style = {
     dashboardBox: {
-        display: 'flex',
-        width: '242px',
-        height: '340px',
-        flexWrap: 'wrap',
+        width: '500px',
+        height: '250px',
         borderRadius: '8px',
         p: '10px',
         color: '#fff',
         backgroundColor: 'color.germanyBox',
-        justifyContent: 'space-between',
-        alignItems: 'center',
         mt: '18px',
         position: 'absolute',
         right: '30px',

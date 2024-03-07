fab8fe189d3b8afd80f4df2eed0c8395446520f7
AnJeMin <jmahn@acrofuture.com>
2024-03-07 13:28:20 +0900

    Fix: 오타 수정

--- src/common/libs/parseMenuPermissionCode.js
+++ src/common/libs/parseMenuPermissionCode.js
@@ -1,7 +1,7 @@
 import menuTree from '#/assets/data/menuTree.json'
 
 /**
- * 권한 코드("M101R")를 메뉴 트리 객체와 권한("R", "U", "A")으로 파싱하는 유틸 함수
+ * 권한 코드("M101R")를 메뉴 트리 객체와 권한("C", "R", "U", "D", "A")으로 파싱하는 유틸 함수
  * @param {string} menuPermissionCode
  * @returns {object} { menuObj, permissionCode }
  */

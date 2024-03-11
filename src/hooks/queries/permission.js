c1656849b8f243b8d24236f449718e328aef7870
AnJeMin <jmahn@acrofuture.com>
2024-03-11 15:02:41 +0900

    Feat: queryKey 작성

--- src/hooks/queries/permission.js
+++ src/hooks/queries/permission.js
@@ -1,10 +1,12 @@
 import { useQuery } from '@tanstack/react-query'
 import permission from '#/api/permission'
+import { QUERY_KEYS } from '#/contents/queryKeys'
 
 export const useGetRoleMenuPermission = () => {
     const { data } = useQuery({
         queryFn: permission.getRoleMenuPermission,
+        queryKey: [QUERY_KEYS.permission.roleMenuPermission],
     })
-    console.log(data)
+
     return { data }
 }

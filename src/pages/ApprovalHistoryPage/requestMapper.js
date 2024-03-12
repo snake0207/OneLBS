4a4b6a61964167aeff5aa538cc90ec14eb986848
JinuYong <amazinggal27@gmail.com>
2024-03-12 16:57:29 +0900

    Feat: request mapper 구현

--- src/pages/ApprovalHistoryPage/requestMapper.js
+++ src/pages/ApprovalHistoryPage/requestMapper.js
@@ -1,38 +1,62 @@
-import {
-    parseCategory,
-    parseCongestion,
-    parseOpeningHours,
-    parsePoiProviderType,
-    parsePriceIsFree,
-} from '#/common/libs/approvalParser.js'
-import t from '#/common/libs/trans.js'
-
 // TODO: null processing
 const evChargerInfo = (data) => {
-    return {}
-}
+    return Object.keys(data).reduce((acc, key) => {
+        if (key === 'parkingFee') acc['pFee'] = data[key]
+        else if (key === 'chargers') acc['charger'] = data[key]
+        else acc[key] = data[key]
 
-const fuelInfo = (data) => {
-    return {}
+        return acc
+    }, {})
 }
 
 const parkingInfo = (data) => {
-    return {}
+    return Object.keys(data).reduce((acc, key) => {
+        if (key === 'brand') acc['company'] = data[key]
+        else if (key === 'type') acc['parkingType'] = data[key]
+        else acc[key] = data[key]
+
+        return acc
+    }, {})
 }
 
 const h2ChargingInfo = (data) => {
-    return {}
+    return Object.keys(data).reduce((acc, key) => {
+        if (key === 'chargers') acc['charger'] = data[key]
+        else acc[key] = data[key]
+
+        return acc
+    }, {})
 }
 
 const dealerInfo = (data) => {
-    return {}
+    return Object.keys(data).reduce((acc, key) => {
+        if (key === 'type') acc['dealerType'] = data[key]
+        else if (key === 'manufacturer') acc['type'] = data[key]
+
+        return acc
+    }, {})
 }
 
 /**
  * POI Detail Request Mapper
+ * @returns {{approvalInfo: *, poiId: *, category: *, status: string, basicInfo: {address: *, position: *, title: *}, evCharger?: *, fuel?: *, parking?: *, h2Charging?: *, dealer?: *}}
+ * @param id
  * @param req
- * @returns {{approvalInfo: *, poiId: *, category: *, status: string, basicInfo: {address: *, position: *, title: *}, evChargerInfo?: *, fuelInfo?: *, parkingInfo?: *, h2ChargingInfo?: *, dealerInfo?: *}}
  */
-const detailRequestDataMapper = (req) => {}
+const detailRequestDataMapper = (id, req) => {
+    if (!id || !req) return
+
+    return {
+        requestId: parseInt(id),
+        editData: {
+            ...req,
+            ...(req.evChargingInfo && { evCharging: evChargerInfo(req.evChargingInfo) }),
+            ...(req.fuelInfo && { fuel: req.fuelInfo }),
+            ...(req.parkingInfo && { parking: parkingInfo(req.parkingInfo) }),
+            ...(req.h2ChargingInfo && { h2Charging: h2ChargingInfo(req.h2ChargingInfo) }),
+            ...(req.dealerPoiInfo && { dealerPoi: dealerInfo(req.dealerPoiInfo) }),
+        },
+    }
+}
 
 export { detailRequestDataMapper }

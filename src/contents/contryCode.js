c5bb8102efa8d9c63da52bc4d87c72a0c39a5a67
강동훈 <dhkang@acrofuture.com>
2024-03-12 18:24:17 +0900

    Feat: 국가코드 전역으로 추출및 alpha2추가, 검색 설정 변경

---
+++ src/contents/contryCode.js
@@ -0,0 +1,53 @@
+export const europeanCountries = [
+    { alpha3: 'ALA', alpha2: 'AX' }, // 알란드 제도
+    { alpha3: 'ALB', alpha2: 'AL' }, // 알바니아
+    { alpha3: 'AND', alpha2: 'AD' }, // 안도라
+    { alpha3: 'AUT', alpha2: 'AT' }, // 오스트리아
+    { alpha3: 'BEL', alpha2: 'BE' }, // 벨기에
+    { alpha3: 'BGR', alpha2: 'BG' }, // 불가리아
+    { alpha3: 'BIH', alpha2: 'BA' }, // 보스니아 헤르체고비나
+    { alpha3: 'BLR', alpha2: 'BY' }, // 벨라루스
+    { alpha3: 'CHE', alpha2: 'CH' }, // 스위스
+    { alpha3: 'CYP', alpha2: 'CY' }, // 키프로스
+    { alpha3: 'CZE', alpha2: 'CZ' }, // 체코
+    { alpha3: 'DEU', alpha2: 'DE' }, // 독일
+    { alpha3: 'DNK', alpha2: 'DK' }, // 덴마크
+    { alpha3: 'ESP', alpha2: 'ES' }, // 스페인
+    { alpha3: 'EST', alpha2: 'EE' }, // 에스토니아
+    { alpha3: 'FIN', alpha2: 'FI' }, // 핀란드
+    { alpha3: 'FRA', alpha2: 'FR' }, // 프랑스
+    { alpha3: 'FRO', alpha2: 'FO' }, // 페로 제도
+    { alpha3: 'GBR', alpha2: 'GB' }, // 영국
+    { alpha3: 'GGY', alpha2: 'GG' }, // 건지 섬
+    { alpha3: 'GIB', alpha2: 'GI' }, // 지브롤터
+    { alpha3: 'GRC', alpha2: 'GR' }, // 그리스
+    { alpha3: 'HRV', alpha2: 'HR' }, // 크로아티아
+    { alpha3: 'HUN', alpha2: 'HU' }, // 헝가리
+    { alpha3: 'IMN', alpha2: 'IM' }, // 맨 섬
+    { alpha3: 'IRL', alpha2: 'IE' }, // 아일랜드
+    { alpha3: 'ISL', alpha2: 'IS' }, // 아이슬란드
+    { alpha3: 'ITA', alpha2: 'IT' }, // 이탈리아
+    { alpha3: 'JEY', alpha2: 'JE' }, // 저지 섬
+    { alpha3: 'KOS', alpha2: 'XK' }, // 코소보
+    { alpha3: 'LTU', alpha2: 'LT' }, // 리투아니아
+    { alpha3: 'LUX', alpha2: 'LU' }, // 룩셈부르크
+    { alpha3: 'LVA', alpha2: 'LV' }, // 라트비아
+    { alpha3: 'MCO', alpha2: 'MC' }, // 모나코
+    { alpha3: 'MDA', alpha2: 'MD' }, // 모르도바
+    { alpha3: 'MNE', alpha2: 'ME' }, // 몬테네그로
+    { alpha3: 'MKD', alpha2: 'MK' }, // 북마케도니아
+    { alpha3: 'MLT', alpha2: 'MT' }, // 말타
+    { alpha3: 'NLD', alpha2: 'NL' }, // 네덜란드
+    { alpha3: 'NOR', alpha2: 'NO' }, // 노르웨이
+    { alpha3: 'POL', alpha2: 'PL' }, // 폴란드
+    { alpha3: 'PRT', alpha2: 'PT' }, // 포르투갈
+    { alpha3: 'ROU', alpha2: 'RO' }, // 루마니아
+    { alpha3: 'RUS', alpha2: 'RU' }, // 러시아
+    { alpha3: 'SMR', alpha2: 'SM' }, // 산마리노
+    { alpha3: 'SRB', alpha2: 'RS' }, // 세르비아
+    { alpha3: 'SVK', alpha2: 'SK' }, // 슬로바키아
+    { alpha3: 'SVN', alpha2: 'SI' }, // 슬로베니아
+    { alpha3: 'SWE', alpha2: 'SE' }, // 스웨덴
+    { alpha3: 'UKR', alpha2: 'UA' }, // 우크라이나
+    { alpha3: 'VAT', alpha2: 'VA' }, // 바티칸 시국
+]

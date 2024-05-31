6273bf3b4e0c71e35dd6b78c8f73c57a4e862e51
이종범 <snake0207@gmail.com>
2024-05-31 16:32:25 +0900

    build 시험

---
+++ assets/index-DXlJ_WV9.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Zwjsjf_e.js";import{F as n,C as x}from"./FormControlLabel-BYqcm0yL.js";function i({checked:r,onChange:s,label:t="",...e}){return o.jsx(n,{onChange:s,control:o.jsx(x,{checked:r,...e}),label:o.jsx("span",{style:{fontSize:"14px"},children:t})})}export{i as C};
